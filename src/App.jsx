aede20bb3a60d712c6b564200cdc2ce79aa2f33d
pnson1322 <pnson22@gmail.com>
2025-10-07 14:30:27 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Route, Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element=""></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
